0520d0be2d47e89a02e14078ef8a8985e422aafc
Gemma Lewington <g.a.lewington@gmail.com>
2024-03-02 20:55:50 +0000

    stuff

---
+++ functions.js
@@ -0,0 +1,48 @@
+function changeColourTest() {
+    document.getElementById("button-generator").style.backgroundColor = "green";
+}
+
+// function to return Y or N for a jammer based on a random number
+function includeJammer() {
+
+    // random number between 1 and 3
+    // odd number = Y
+    // even number = N
+    // return Y or N
+    
+
+    console.log("includeJammer");
+}
+
+// function to return Y or N for a pivot based on a random number
+function includePivot() {
+
+    // random number between 1 and 3
+    // odd number = Y
+    // even number = N
+    // return Y or N
+}
+
+// function to return a random number of blockers
+function numberOfBlockers() {
+
+    // random number between 1 and 100
+    // 1-10 = 0
+    // 11-25 = 1
+    // 26-60 = 2
+    // 61-100 = 3
+    // return number
+}
+
+function generatePack() {
+    let pack = 1;
+    // call includeJammer
+    // call includePivot
+    // call numberOfBlockers
+    
+    console.log(pack);
+
+    // return pack
+    return pack;
+    
+}
